1f8745eef46d25d95b31446c96d85c74d4e15fb0
kyeongmin1011 <kyeongmin21@naver.com>
2022-12-08 10:09:36 +0900

    header, footer 컴포넌트 생성

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,17 @@
 <template>
-  <div>
-
+  <div class="header">
+    <ul class="gnb">
+      <li><a href="">CHALLENGE</a></li>
+      <li><a href="">MAGAZINE</a></li>
+      <li><a href="">DAILYLOOK</a></li>
+      <li><a href="">EVENT</a></li>
+    </ul>
+    <ul class="snb">
+      <li><a href="">검색</a></li>
+      <li><a href="">로그인</a></li>
+      <li><a href="">알림</a></li>
+      <li><a href="">글쓰기</a></li>
+    </ul>
   </div>
 </template>
 
