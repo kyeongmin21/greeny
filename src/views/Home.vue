76a8ccad541480c14252794e12a30967f7c897e4
kyeongmin1011 <kyeongmin21@naver.com>
2022-12-08 17:20:30 +0900

    feat: home-베스트챌린지 레이아웃
    issue: #1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,29 @@
 <template>
   <div class="home">
-    <h2>홈</h2>
+
+    <section>
+      <div class="best_challenge">
+        <h2><img src="/images/home/title01.png" alt="챌린지 타이틀"></h2>
+        <ul>
+          <li>
+            <img src="/images/home/home_img01.png" alt="챌린지 이미지1">
+            <p><b>[한달나기] 블랙 코트 하나로 일주일 코디하기</b></p>
+            <span>자꾸자꾸 손이 가는 겨울 데일리 아이템! 블랙 코트로 스타일링 꿀!팁!</span>
+          </li>
+          <li>
+            <img src="/images/home/home_img02.png" alt="챌린지 이미지2">
+            <p><b>[일주일챌린지] 최애 카페에서 텀블러 이용해서 할인받고 선물도 받기</b></p>
+            <span>최애 카페에서 제공하는 텀블러 이벤트에 참여해서 할인도 받고 선물도...</span>
+          </li>
+        </ul>
+      </div>
+
+      <div class="more">MORE</div>
+    </section>
+
+
+
+
   </div>
 </template>
 
