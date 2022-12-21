f77db738a05a7b6a9f4b55d988e370fc35011ea1
kyeongmin1011 <kyeongmin@safety-as.com>
2022-12-21 11:25:35 +0900

    fix: header, footer 반응형 기능 구현
    -가로스크롤 버그 해결 : body,html-overflow
    -햄버거버튼 반응형
    
    issue: #22

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -2,9 +2,7 @@
   <div class="Menu">
     <ul class="navbar_menu">
       <li @click="modalSearch" v-b-modal.modal-search><img src="/images/svg/search.svg" alt="검색"></li>
-      <li>
-        <router-link to="/login"><img src="/images/svg/my.svg" alt="마이페이지"></router-link>
-      </li>
+      <li><router-link to="/login"><img src="/images/svg/my.svg" alt="마이페이지"></router-link></li>
       <li><img src="/images/svg/noti.svg" alt="알림"></li>
       <li><img src="/images/svg/write.svg" alt="글쓰기"></li>
     </ul>
