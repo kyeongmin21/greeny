3c0152813341e887d3c22e6f0405431f55283e57
kyeongmin1011 <kyeongmin@safety-as.com>
2023-01-02 14:34:00 +0900

    feat: 매거진 카테고리 반응형
    issue: #47

--- src/views/magazine/List.vue
+++ src/views/magazine/List.vue
@@ -4,6 +4,33 @@
       <img src="/images/magazine_h1.png" alt="매거진타이틀">
     </div>
 
+
+    <div class="magazine_wrap">
+      <section>
+        <div>
+          <ul>
+            <li>
+              <img src="/images/magazine_banner01.jpg" alt="매거진썸네일1">
+            </li>
+            <li>
+              <img src="/images/magazine_banner02.jpg" alt="매거진썸네일2">
+            </li>
+            <li>
+              <img src="/images/magazine_banner03.jpg" alt="매거진썸네일3">
+            </li>
+            <li>
+              <img src="/images/magazine_banner04.jpg" alt="매거진썸네일4">
+            </li>
+            <li>
+              <img src="/images/magazine_banner05.jpg" alt="매거진썸네일5">
+            </li>
+            <li>
+              <img src="/images/magazine_banner06.jpg" alt="매거진썸네일6">
+            </li>
+          </ul>
+        </div>
+      </section>
+    </div>
   </div>
 </template>
 
